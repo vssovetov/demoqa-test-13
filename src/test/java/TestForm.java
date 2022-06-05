c9b282e55b502ff9669cbd537590653c3735d772
VSsovetov92 <VSsovetov@yandex.ru>
2022-06-05 16:50:38 +0400

    create TestBase

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,16 +1,9 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-    public class testForm {
+    public class TestForm extends TestBase{
 
-        @BeforeAll
-        static void beforeAll() {
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = "1920x1080";
-        }
 
         @Test
         void successfulTest() {
